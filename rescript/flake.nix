ffd5264f5ef6621dc055c99a4e28c31db9db1730
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2024-05-24 19:40:29 +0900

    flake for rescript projects

---
+++ rescript/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "rescript project flake";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=23.11";
+    rescript-lsp = {
+      url = "github:MattBrooks95/rescript-lsp-flake?ref=support-macos";
+    };
+  };
+
+  outputs = { self, nixpkgs, rescript-lsp, ... }@inputs:
+  let
+      forAllSystems = (function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "x86_64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system} system));
+  in
+  {
+    devShells = forAllSystems(pkgs: system:
+      let shell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            rescript-lsp.packages.${system}.default
+          ];
+        };
+      in {
+        default = shell;
+      }
+    );
+  };
+}
